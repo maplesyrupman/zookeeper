24f3b73ff43bfc81c38d58f85d80d4c330089382
maplesyrupman <maplesyrupman@protonmail.com>
2021-10-26 23:41:44 -0400

    changed PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { animals } = require('./data/animals.json');
 
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const filterByQuery = (query, animalsArray) => {
@@ -40,7 +40,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results)
 })
 
-app.listen(3001, () => {
-    console.log(`API server now on port ${3001}`)
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`)
 })
 
